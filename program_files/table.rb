46536f53401a5464a074717b3cd16f00aad72036
LiGM-GH <lipkin.gregory@outlook.com>
2022-01-22 00:04:13 +0300

    Table view set

--- program_files/table.rb
+++ program_files/table.rb
@@ -24,28 +24,80 @@ class Table
     args.each { |arg| @lines << arg }
   end
 
+  ##
+  # Returns pretty formatted table.
   def to_s(vertical_border: '|', horizontal_border: '-')
     keys = []
+    vals = []
     @lines.each do |line|
-      line.each_key { |key| keys << key unless keys.include?(key) }
+      line.each_pair do |key, val| 
+        keys << key unless keys.include?(key)
+        vals << val unless vals.include?(val)
+      end
     end
     border_width = vertical_border.chomp.length
     border_cover = horizontal_border
-    cell_length = keys.max_by { |key| key.length }.length
-
-    table_to_s = vertical_border +
+    cell_length_keys = keys.max_by { |key| key.length }.length + 2 * border_width
+    cell_length_vals = vals.max_by { |val| val.to_s.length }.length + 2 * border_width
+    cell_length = [cell_length_vals, cell_length_keys].max
+    str = vertical_border +
                   keys.map { |key|
-                    key.to_s.center(key.length + border_width)\
-                      .center(cell_length + 2 * border_width, border_cover)
+                    key.to_s.center(key.length + 2 * border_width)\
+                      .center(cell_length, border_cover)
                   }.join(vertical_border) +
                   vertical_border
+
+    line_length = str.length
+
+    str += empty_line(length:             cell_length,
+                      left_border:    vertical_border,
+                      center_border:  vertical_border,
+                      right_border:   vertical_border,
+                      cover:        horizontal_border,
+                      times:              keys.length)
+    @lines.each_with_index do |line, i|
+      str += tabled_line(line: line, keys: keys, length: cell_length)
+    end
+    str
+  end
+
+  def tabled_line(line:           {},
+                  keys:           [],
+                  left_border:   "|",
+                  center_border: "|",
+                  right_border:  "|",
+                  cover:         "-",
+                  length:         10)
+    keys.empty? && raise(ArgumentError.new('Empty param :keys in tabled_line'))
+    str = "\n"
+    str += left_border
+    keys.each_with_index do |key, index|
+      if line.key?(key)
+        str += line[key].to_s.center(length)
+      else
+        str += "".ljust(length, cover)
+      end
+      index == keys.length || str += center_border 
+    end
+    str += ""
+  end
+
+  def empty_line( left_border:   '|',
+                  center_border: '|',
+                  right_border:  '|',
+                  cover:         '-',
+                  length:         10,
+                  times:           1)
+    str = "\n"
+    str += left_border
+    (times - 1).times do
+      str += "".ljust(length, cover)
+      str += center_border
+    end
+    str += "".ljust(length, cover)
     
-    line_length = table_to_s.length
-    table_to_s += "\n" + vertical_border + 
-                  "".ljust(line_length - 2 * border_width, border_cover) + 
-                  vertical_border + "\n"
-    table_to_s
+    str += right_border
+    str += ""
   end
 end
-a = Table.new({a: 2, b: 3})
-puts a
+puts Table.new({a: 2, b: 3, c: 'Alexandro'}, {k: 3})
